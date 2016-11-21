661d849cd6cf1e65a1059b1e441f77c79ab1c7fe
JuheeHyun <j.hyun091@gmail.com>
2016-11-21 19:55:05 +0900

    [fender]전체 페이지 수정본 2차
    16.11.21 html, css, js 병합

--- fender/js/src/show_up.js
+++ fender/js/src/show_up.js
@@ -1,5 +1,8 @@
 //show_up.js
 $(function(){
+
+//메인페이지
+
 var bg_box_01 =  $('.bg_box').fadeIn(3000);
 var menu = $('#menu').delay(1000).fadeIn(500);
 var logo = $('#topBtn').delay(1000).fadeIn(500);
@@ -18,4 +21,22 @@ var snb_04 = $('#snb').find('li').eq(3).delay(3000).fadeIn(1000);
 var snb_05 = $('#snb').find('li').eq(4).delay(3000).fadeIn(1000);
 var scroll = $('#scrollDown').delay(3200).fadeIn(1000);
 	
+// neck
+var neck_tit = $('#neckBox').find('#neckTitleBox').delay(1500).fadeIn(500);
+// about
+var about_tit = $('#aboutBox').find('#aboutTitleBox').delay(1500).fadeIn(500);
+var about_txt = $('#aboutBox').find('#aboutTextBox').delay(2000).fadeIn(1500);
+var about_btn = $('#aboutBox').find('button').delay(2500).fadeIn(1500);
+
+
+
+
+ // 링크연결 
+ $('.mnbTextUl>li').on('click',function(e){
+ 	$('#mnb').fadeOut(1000,function(){
+ 		$('content').fadeOut(1000);
+ 		});
+ 	});
+
 });
+
